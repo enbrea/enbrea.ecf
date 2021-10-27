68a83fb1a7cd07122ad4e572022aebe3de917eed
Frank Stüber <frank@stueber.de>
2021-10-27 12:00:05 +0200

    Dependency update

---
+++ src/EcfDictionary.cs
@@ -0,0 +1,26 @@
+﻿#region ENBREA ECF - Copyright (C) 2021 STÜBER SYSTEMS GmbH
+/*    
+ *    ENBREA ECF
+ *    
+ *    Copyright (C) 2021 STÜBER SYSTEMS GmbH
+ *
+ *    Licensed under the MIT License, Version 2.0. 
+ * 
+ */
+#endregion
+
+using Enbrea.Csv;
+
+namespace Enbrea.Ecf
+{
+    public class EcfDictionary : CsvDictionary
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="EcfDictionary"/> class.
+        /// </summary>
+        public EcfDictionary()
+            : base(new EcfConverterResolver())
+        {
+        }
+    }
+}
